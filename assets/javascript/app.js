f8936933aa390d46d66cef9608fe4600a0628e43
Alex Trilling <atrill90@gmail.com>
2018-04-03 18:03:03 -0400

    questions added

---
+++ assets/javascript/app.js
@@ -0,0 +1,57 @@
+var questions =[
+    { 
+    question:'How many Stark children are there at the start of the show?',
+    choices :[4, 5, 6, 7] ,
+    answer: 5
+    },
+   { 
+    question:'How many children does Cersei have at the start of the show?',
+    choices :[0, 2, 4, 3] ,
+    answer: 3
+    },
+    { 
+    question:'What continent is the seven kingdoms on?',
+    choices :["Easteros", "Planetos", "Westeros", "Asshai"] ,
+    answer: 5
+    },
+    { 
+    question:'Another name for Oberyn Martell is?',
+    choices :["the viper", "the lion", "the shark", "the cobra"] ,
+    answer: 5
+    },
+    { 
+    question:'How many direwolf pups are there at the start of the show',
+    choices :[4, 5, 6, 7] ,
+    answer: 6
+    },
+    { 
+    question:'What was the Mad King\'s first name',
+    choices :["Aemon", "Aerys", "Aegon", "Armon"] ,
+    answer: "Aerys"
+    },
+    { 
+    question:'What is the name of the leader of the dead',
+    choices :["Winter King", "Nightmare", "Night King", "Frosty the Snowman"] ,
+    answer: 5
+    },
+    { 
+    question:'What is Arya\'s sword',
+    choices :["Needler", "Needle", "Pointy", "Mr. Stick"] ,
+    answer: "Needle"
+    },
+    { 
+    question:'What are the Greyjoy\'s words',
+    choices :["We Do Not Bow", "Family,Duty,Honor", "We Do Not Sow", "Winter is Coming"] ,
+    answer: "We Do Not Sow"
+    },
+    { 
+    question:'What is Jon Snow\'s sword name ',
+    choices :["Longclaw", "Sharpclaw", "Longpaw", "Bastardclaw"] ,
+    answer: 5
+    },
+    
+
+
+
+
+];
